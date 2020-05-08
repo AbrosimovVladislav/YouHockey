298dc2a63f29a5c88107907c9041ec1562328c09
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-05-08 17:58:33 +0300

    YH-16: brand dataflow adding

---
+++ src/main/java/ru/yourhockey/service/BrandService.java
@@ -0,0 +1,20 @@
+package ru.yourhockey.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yourhockey.model.product_attributes.Brand;
+import ru.yourhockey.repo.BrandRepo;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BrandService {
+
+    private final BrandRepo brandRepo;
+
+    public List<Brand> getAll(){
+        return brandRepo.findAll();
+    }
+
+}
---
+++ src/main/java/ru/yourhockey/web/controller/BrandController.java
@@ -0,0 +1,22 @@
+package ru.yourhockey.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yourhockey.model.product_attributes.Brand;
+import ru.yourhockey.service.BrandService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BrandController {
+
+    private final BrandService brandService;
+
+    @GetMapping("/brands")
+    public List<Brand> brands() {
+        return brandService.getAll();
+    }
+
+}
