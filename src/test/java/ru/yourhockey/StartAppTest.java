d346cc48994e80fc4bf24befdf4d352f8431dfc9
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-05-28 17:41:05 +0300

    Add basic test

---
+++ src/test/java/ru/yourhockey/StartAppTest.java
@@ -0,0 +1,10 @@
+package ru.yourhockey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartAppTest {
+    @Test
+    void contextLoads() {}
+}
