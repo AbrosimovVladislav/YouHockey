81b496e68395befcee0f03c7f48a5fb518ac9b43
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-06-28 11:31:50 +0300

    Merge branch 'develop' of C:\Vlad\ProgFolder\IdeaProjects\YouHockey with conflicts.

--- src/main/java/ru/yourhockey/web/controller/ProductApiController.java
+++ src/main/java/ru/yourhockey/web/controller/ProductApiController.java
@@ -47,6 +47,7 @@ public class ProductApiController implements ProductApi {
                                            @PageableDefault(size = DEFAULT_PAGE_SIZE, page = DEFAULT_PAGE_NUMBER) Pageable pageable) {
         String inStock = requestParams.remove("inStock");
         String priceFilter = requestParams.get("offer.price");
+        //ToDo переименовать в sort and page extractor
         FilterAndPageable pairOfParamsAndPageable = validator.validate(requestParams, pageable, Product.class);
         pairOfParamsAndPageable = validator.validateAgeParam(pairOfParamsAndPageable);
         List<Product> products;
