912b0b93eee15ae9151feed02a27d4f494a54b98
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-07-08 18:00:20 +0300

    YH-198: deployment test commit

--- src/main/java/ru/yourhockey/MainController.java
+++ src/main/java/ru/yourhockey/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    //deployment test comment
     @GetMapping("/")
     public String index() {
         return "redirect:swagger-ui.html";
