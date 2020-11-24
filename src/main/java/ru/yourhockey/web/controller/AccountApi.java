61b89c2d4ded82be100317f9de6d0df04bf44cbb
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-11-24 19:29:37 +0300

    master: pre final data loading

--- src/main/java/ru/yourhockey/web/controller/AccountApi.java
+++ src/main/java/ru/yourhockey/web/controller/AccountApi.java
@@ -1,15 +1,15 @@
-package ru.yourhockey.web.controller;
-
-import io.swagger.annotations.ApiOperation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import ru.yourhockey.model.account.Account;
-
-public interface AccountApi {
-
-    @ApiOperation(value = "Add account",
-            notes = "Add new account",
-            response = ResponseEntity.class)
-    ResponseEntity<Account> addAccount(Account account, BindingResult bindingResult);
-
-}
+//package ru.yourhockey.web.controller;
+//
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.BindingResult;
+//import ru.yourhockey.model.account.Account;
+//
+//public interface AccountApi {
+//
+//    @ApiOperation(value = "Add account",
+//            notes = "Add new account",
+//            response = ResponseEntity.class)
+//    ResponseEntity<Account> addAccount(Account account, BindingResult bindingResult);
+//
+//}
