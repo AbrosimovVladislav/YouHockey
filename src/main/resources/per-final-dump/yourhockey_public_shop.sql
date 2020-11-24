3ffc12b89fcbcf6de499d131f437718fc85f4c3f
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-11-24 19:24:42 +0300

    master: pre final data loading

---
+++ src/main/resources/per-final-dump/yourhockey_public_shop.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (1, 'HOCKEYBEZGRANIC', null, 'https://hockeybezgranic.ru');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (2, 'SPORTDEPO', null, 'https://www.sportdepo.ru/');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (3, 'HOCK5', null, 'https://hock5.ru/');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (4, 'ALLHOCKEY', null, 'https://allhockey24.ru');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (5, 'FORMASPB', null, 'https://forma-spb.ru');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (6, 'KLUSHKI', null, 'https://klushki.com//');
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (7, 'NORDHOCKEY', null, 'https://www.nordhockey.ru');
